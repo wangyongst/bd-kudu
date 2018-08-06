999454f7718e4d9ee98ba23972ea565bb7506c4b
wangyongst <wangyongst@gmail.com>
2018-08-06 18:00:21 +0800

    office

--- src/main/java/com/myweb/kafka/Listener.java
+++ src/main/java/com/myweb/kafka/Listener.java
@@ -1,19 +1,43 @@
 package com.myweb.kafka;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.myweb.elasticsearch.dao.DepthPriceRawRepository;
+import com.myweb.elasticsearch.dao.TradeHistoryRawRepository;
+import com.myweb.pojo.DepthPriceRaw;
+import com.myweb.pojo.TradeHistoryRaw;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.annotation.KafkaListener;
-import org.springframework.stereotype.Component;
+
+import java.io.IOException;
 
 public class Listener {
 
+    @Autowired
+    private DepthPriceRawRepository depthPriceRawRepository;
+
+    @Autowired
+    private TradeHistoryRawRepository tradeHistoryRawRepository;
+
     @KafkaListener(topics = {"depth-price-raw"})
     public void depthPriceRaw(ConsumerRecord<?, ?> record) {
-        System.out.println(record.value().toString());
+        ObjectMapper mapper = new ObjectMapper();
+        try {
+            DepthPriceRaw depthPriceRaw = mapper.readValue(record.value().toString(), DepthPriceRaw.class);
+            depthPriceRawRepository.save(depthPriceRaw);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     @KafkaListener(topics = {"trade-history-raw"})
     public void tradeHistoryRaw(ConsumerRecord<?, ?> record) {
-        System.out.println(record.value().toString());
+        ObjectMapper mapper = new ObjectMapper();
+        try {
+            TradeHistoryRaw tradeHistoryRaw = mapper.readValue(record.value().toString(), TradeHistoryRaw.class);
+            tradeHistoryRawRepository.save(tradeHistoryRaw);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
