19471a97ce5a188ade7ebc4060ca0c6fd5bbfd73
wangyongst <wangyongst@gmail.com>
2018-08-06 17:51:52 +0800

    office

--- src/main/java/com/boot/spring/boot/MainApplication.java
+++ src/main/java/com/boot/spring/boot/MainApplication.java
@@ -3,14 +3,16 @@ package com.boot.spring.boot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 
 /**
  * Created by BHWL on 2017-08-09.
  */
 @SpringBootApplication
-public class MainApplication extends SpringBootServletInitializer {
+@ComponentScan(basePackages = {"com.myweb.kafka"})
+@ComponentScan(basePackages = {"com.myweb.elasticsearch.dao"})
+@ComponentScan(basePackages = {"com.myweb.pojo"})
+public class MainApplication {
     public static void main(String[] args) throws Exception {
         SpringApplication.run(MainApplication.class, args);
     }
--- src/main/java/com/myweb/kafka/Listener.java
+++ src/main/java/com/myweb/kafka/Listener.java
@@ -1,18 +1,11 @@
 package com.myweb.kafka;
 
-import com.myweb.elasticsearch.dao.DepthPriceRawRepository;
-import com.myweb.elasticsearch.dao.TradeHistoryRawRepository;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
 public class Listener {
-    @Autowired
-    private DepthPriceRawRepository depthPriceRawRepository;
-
-    @Autowired
-    private TradeHistoryRawRepository tradeHistoryRawRepository;
 
     @KafkaListener(topics = {"depth-price-raw"})
     public void depthPriceRaw(ConsumerRecord<?, ?> record) {
