97ff57d1c3a43c579610b4776e0549c1c80ad6c3
wangyongst <wangyongst@gmail.com>
2018-08-06 11:02:54 +0800

    office

--- src/main/java/com/myweb/kafka/Listener.java
+++ src/main/java/com/myweb/kafka/Listener.java
@@ -7,14 +7,14 @@ import org.springframework.kafka.annotation.KafkaListener;
 
 public class Listener {
     //protected final Logger logger = LoggerFactory.getLogger(this.getClass());
-    @KafkaListener(topics = {"test"})
-    public void listenReal(ConsumerRecord<?, ?> record) {
+    @KafkaListener(topics = {"depth-price-raw"})
+    public void depthPriceRaw(ConsumerRecord<?, ?> record) {
         System.out.println("kafka的key: " + record.key());
         System.out.println("kafka的value: " + record.value().toString());
     }
 
-    @KafkaListener(topics = {"test"})
-    public void listenHistory(ConsumerRecord<?, ?> record) {
+    @KafkaListener(topics = {"trade-history-raw"})
+    public void tradeHistoryRaw(ConsumerRecord<?, ?> record) {
         System.out.println("kafka的key: " + record.key());
         System.out.println("kafka的value: " + record.value().toString());
     }
