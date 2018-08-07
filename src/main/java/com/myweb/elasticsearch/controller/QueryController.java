4e452ff73453c610d660984dde49d7cc121dfea4
wangyongst <wangyongst@gmail.com>
2018-08-07 17:44:09 +0800

    office

--- src/main/java/com/myweb/elasticsearch/controller/QueryController.java
+++ src/main/java/com/myweb/elasticsearch/controller/QueryController.java
@@ -2,16 +2,22 @@ package com.myweb.elasticsearch.controller;
 
 import com.myweb.elasticsearch.dao.DepthPriceRawRepository;
 import com.myweb.elasticsearch.dao.TradeHistoryRawRepository;
+import com.myweb.pojo.DepthPriceRaw;
+import com.myweb.pojo.TradeHistoryRaw;
 import com.myweb.vo.Parameter;
 import com.myweb.vo.Result;
 import org.apache.commons.lang3.StringUtils;
+import org.elasticsearch.index.query.QueryStringQueryBuilder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Iterator;
 
 @RestController
 public class QueryController {
@@ -21,8 +27,14 @@ public class QueryController {
     @Autowired
     private TradeHistoryRawRepository tradeHistoryRawRepository;
 
-    @RequestMapping("/query")
+    @PostMapping("/query")
     public Object queryDepthPriceRaw(@RequestBody Parameter parameter) {
+        if (StringUtils.isBlank(parameter.getTableName()) || (!parameter.getTableName().equals("depth-price-raw") && !parameter.getTableName().equals("trade-history-raw"))) {
+            Result result = new Result();
+            result.setStatus(20001);
+            result.setMessage("The parameter tableName must been depth-price-raw or trade-history-raw");
+            return result;
+        }
         if (parameter.getPage() == null || parameter.getPage().intValue() == 0) parameter.setPage(0);
         if (parameter.getPagesize() == null || parameter.getPagesize().intValue() == 0) parameter.setPagesize(10);
         Pageable pageable = null;
@@ -32,28 +44,40 @@ public class QueryController {
         } else if (StringUtils.isBlank(parameter.getOrder()) || parameter.getOrder().equals("asc")) {
             sort = new Sort(Sort.Direction.ASC, "timestamp");
         }
-        pageable = new PageRequest(parameter.getPage().intValue(), parameter.getPagesize().intValue(), sort);
-        if (StringUtils.isBlank(parameter.getTableName())) {
-            Result result = new Result();
-            result.setStatus(20001);
-            result.setMessage("The parameter tableName can not be empty");
-            return result;
-        } else {
-            if (parameter.getTableName().equals("depth-price-raw")) {
-                if (parameter.getCounterparty().size() == 0 && parameter.getSymbol().size() == 0 && parameter.getStartTimestamp().intValue() == 0 && parameter.getEndTimestamp().intValue() == 0) {
-                    return depthPriceRawRepository.findAll(pageable);
-                } else if (parameter.getCounterparty().size() == 0 && parameter.getSymbol().size() > 0) {
+        pageable = new PageRequest(parameter.getPage().intValue(), parameter.getPagesize().intValue(), null);
+        if (parameter.getCounterparty() == null && parameter.getSymbol() == null && parameter.getStartTimestamp() == null && parameter.getEndTimestamp() == null) {
+            if (parameter.getTableName().equals("trade-history-raw")) {
+                return depthPriceRawRepository.findAll(pageable);
+            } else return tradeHistoryRawRepository.findAll(pageable);
+        } else if (parameter.getCounterparty().size() == 0 && parameter.getSymbol().size() > 0) {
+            if (parameter.getTableName().equals("trade-history-raw")) {
+                return depthPriceRawRepository.findAllBySymbolInAndTimestampBeforeAndTimestampAfter(parameter.getSymbol(), parameter.getEndTimestamp().intValue(), parameter.getStartTimestamp().intValue(), pageable);
+            } else return tradeHistoryRawRepository.findAllBySymbolInAndTimestampBeforeAndTimestampAfter(parameter.getSymbol(), parameter.getEndTimestamp().intValue(), parameter.getStartTimestamp().intValue(), pageable);
+        } else if (parameter.getCounterparty().size() > 0 && parameter.getSymbol().size() == 0) {
+            if (parameter.getTableName().equals("trade-history-raw")) {
+                return depthPriceRawRepository.findAllByCounterPartyInAndTimestampBeforeAndTimestampAfter(parameter.getCounterparty(), parameter.getEndTimestamp().intValue(), parameter.getStartTimestamp().intValue(), pageable);
+            } else return tradeHistoryRawRepository.findAllByCounterPartyInAndTimestampBeforeAndTimestampAfter(parameter.getCounterparty(), parameter.getEndTimestamp().intValue(), parameter.getStartTimestamp().intValue(), pageable);
+        } else if (parameter.getCounterparty().size() > 0 && parameter.getSymbol().size() > 0) {
+            if (parameter.getTableName().equals("trade-history-raw")) {
+                return depthPriceRawRepository.findAllByCounterPartyInAndSymbolInAndTimestampBeforeAndTimestampAfter(parameter.getCounterparty(), parameter.getCounterparty(), parameter.getEndTimestamp().intValue(), parameter.getStartTimestamp().intValue(), pageable);
+            } else
+                return tradeHistoryRawRepository.findAllByCounterPartyInAndSymbolInAndTimestampBeforeAndTimestampAfter(parameter.getCounterparty(), parameter.getCounterparty(), parameter.getEndTimestamp().intValue(), parameter.getStartTimestamp().intValue(), pageable);
 
-                }
-            } else if (parameter.getTableName().equals("trade-history-raw")) {
-                return tradeHistoryRawRepository.findAll(pageable);
-            } else {
-                Result result = new Result();
-                result.setStatus(20002);
-                result.setMessage("The parameter tableName do not exist");
-                return result;
-            }
         }
         return null;
+    }
+
+    @RequestMapping("/query/depth-price-raw")
+    public Object queryDepthPriceRaw() {
+        Sort sort = new Sort(Sort.Direction.DESC, "timestamp");
+        Pageable pageable = new PageRequest(0, 10, sort);
+        return depthPriceRawRepository.findAll(pageable);
+    }
+
+    @RequestMapping("/query/trade-history-raw")
+    public Object queryTradeHistoryRaw() {
+        Sort sort = new Sort(Sort.Direction.DESC, "timestamp");
+        Pageable pageable = new PageRequest(0, 10, sort);
+        return tradeHistoryRawRepository.findAll(pageable);
     }
 }
--- src/main/java/com/myweb/elasticsearch/dao/TradeHistoryRawRepository.java
+++ src/main/java/com/myweb/elasticsearch/dao/TradeHistoryRawRepository.java
@@ -4,6 +4,15 @@ import com.myweb.pojo.TradeHistoryRaw;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
+
+import java.util.List;
+
 public interface TradeHistoryRawRepository extends ElasticsearchRepository<TradeHistoryRaw, Long> {
     public Page<TradeHistoryRaw> findAll(Pageable pageable);
+
+    public Page<TradeHistoryRaw> findAllBySymbolInAndTimestampBeforeAndTimestampAfter(List<String> symbols, int endTimestamp, int startTimestamp, Pageable pageable);
+
+    public Page<TradeHistoryRaw> findAllByCounterPartyInAndTimestampBeforeAndTimestampAfter(List<String> counterPartys, int endTimestamp, int startTimestamp, Pageable pageable);
+
+    public Page<TradeHistoryRaw> findAllByCounterPartyInAndSymbolInAndTimestampBeforeAndTimestampAfter(List<String> counterPartys, List<String> symbols, int endTimestamp, int startTimestamp, Pageable pageable);
 }
