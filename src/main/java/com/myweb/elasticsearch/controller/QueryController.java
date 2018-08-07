1abb4fd98b78796731bc139e3fc425d52ff29c8b
wangyongst <wangyongst@gmail.com>
2018-08-07 17:49:01 +0800

    office

--- src/main/java/com/myweb/elasticsearch/controller/QueryController.java
+++ src/main/java/com/myweb/elasticsearch/controller/QueryController.java
@@ -35,7 +35,7 @@ public class QueryController {
             result.setMessage("The parameter tableName must been depth-price-raw or trade-history-raw");
             return result;
         }
-        if (parameter.getPage() == null || parameter.getPage().intValue() == 0) parameter.setPage(0);
+        if (parameter.getPage() == null) parameter.setPage(0);
         if (parameter.getPagesize() == null || parameter.getPagesize().intValue() == 0) parameter.setPagesize(10);
         Pageable pageable = null;
         Sort sort = null;
@@ -44,7 +44,7 @@ public class QueryController {
         } else if (StringUtils.isBlank(parameter.getOrder()) || parameter.getOrder().equals("asc")) {
             sort = new Sort(Sort.Direction.ASC, "timestamp");
         }
-        pageable = new PageRequest(parameter.getPage().intValue(), parameter.getPagesize().intValue(), null);
+        pageable = new PageRequest(parameter.getPage().intValue(), parameter.getPagesize().intValue(), sort);
         if (parameter.getCounterparty() == null && parameter.getSymbol() == null && parameter.getStartTimestamp() == null && parameter.getEndTimestamp() == null) {
             if (parameter.getTableName().equals("trade-history-raw")) {
                 return depthPriceRawRepository.findAll(pageable);
@@ -69,7 +69,7 @@ public class QueryController {
 
     @RequestMapping("/query/depth-price-raw")
     public Object queryDepthPriceRaw() {
-        Sort sort = new Sort(Sort.Direction.DESC, "timestamp");
+        Sort sort = new Sort(Sort.Direction.ASC, "timestamp");
         Pageable pageable = new PageRequest(0, 10, sort);
         return depthPriceRawRepository.findAll(pageable);
     }
