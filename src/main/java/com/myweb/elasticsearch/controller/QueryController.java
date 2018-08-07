581b16753454076f67782fd69f9ed336cab96872
wangyongst <wangyongst@gmail.com>
2018-08-07 16:56:09 +0800

    office

--- src/main/java/com/myweb/elasticsearch/controller/QueryController.java
+++ src/main/java/com/myweb/elasticsearch/controller/QueryController.java
@@ -2,10 +2,14 @@ package com.myweb.elasticsearch.controller;
 
 import com.myweb.elasticsearch.dao.DepthPriceRawRepository;
 import com.myweb.elasticsearch.dao.TradeHistoryRawRepository;
+import com.myweb.vo.Parameter;
+import com.myweb.vo.Result;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,17 +21,39 @@ public class QueryController {
     @Autowired
     private TradeHistoryRawRepository tradeHistoryRawRepository;
 
-    @RequestMapping("/query/depth-price-raw")
-    public Object queryDepthPriceRaw() {
-        Sort sort = new Sort(Sort.Direction.DESC, "timestamp");
-        Pageable pageable = new PageRequest(0, 10, sort);
-        return depthPriceRawRepository.findAll(pageable);
-    }
+    @RequestMapping("/query")
+    public Object queryDepthPriceRaw(@RequestBody Parameter parameter) {
+        if (parameter.getPage() == null || parameter.getPage().intValue() == 0) parameter.setPage(0);
+        if (parameter.getPagesize() == null || parameter.getPagesize().intValue() == 0) parameter.setPagesize(10);
+        Pageable pageable = null;
+        Sort sort = null;
+        if (StringUtils.isNotBlank(parameter.getOrder()) && parameter.getOrder().equals("desc")) {
+            sort = new Sort(Sort.Direction.DESC, "timestamp");
+        } else if (StringUtils.isBlank(parameter.getOrder()) || parameter.getOrder().equals("asc")) {
+            sort = new Sort(Sort.Direction.ASC, "timestamp");
+        }
+        pageable = new PageRequest(parameter.getPage().intValue(), parameter.getPagesize().intValue(), sort);
+        if (StringUtils.isBlank(parameter.getTableName())) {
+            Result result = new Result();
+            result.setStatus(20001);
+            result.setMessage("The parameter tableName can not be empty");
+            return result;
+        } else {
+            if (parameter.getTableName().equals("depth-price-raw")) {
+                if (parameter.getCounterparty().size() == 0 && parameter.getSymbol().size() == 0 && parameter.getStartTimestamp().intValue() == 0 && parameter.getEndTimestamp().intValue() == 0) {
+                    return depthPriceRawRepository.findAll(pageable);
+                } else if (parameter.getCounterparty().size() == 0 && parameter.getSymbol().size() > 0) {
 
-    @RequestMapping("/query/trade-history-raw")
-    public Object queryTradeHistoryRaw() {
-        Sort sort = new Sort(Sort.Direction.DESC, "timestamp");
-        Pageable pageable = new PageRequest(0, 10, sort);
-        return tradeHistoryRawRepository.findAll(pageable);
+                }
+            } else if (parameter.getTableName().equals("trade-history-raw")) {
+                return tradeHistoryRawRepository.findAll(pageable);
+            } else {
+                Result result = new Result();
+                result.setStatus(20002);
+                result.setMessage("The parameter tableName do not exist");
+                return result;
+            }
+        }
+        return null;
     }
 }
---
+++ src/main/java/com/myweb/vo/Parameter.java
@@ -0,0 +1,79 @@
+package com.myweb.vo;
+
+import java.util.List;
+
+public class Parameter {
+
+    private String tableName;
+    private List<String> counterparty;
+    private List<String> symbol;
+    private Number startTimestamp;
+    private Number endTimestamp;
+    private Number page;
+    private Number pagesize;
+    private String order;
+
+    public String getTableName() {
+        return tableName;
+    }
+
+    public void setTableName(String tableName) {
+        this.tableName = tableName;
+    }
+
+    public List<String> getCounterparty() {
+        return counterparty;
+    }
+
+    public void setCounterparty(List<String> counterparty) {
+        this.counterparty = counterparty;
+    }
+
+    public List<String> getSymbol() {
+        return symbol;
+    }
+
+    public void setSymbol(List<String> symbol) {
+        this.symbol = symbol;
+    }
+
+    public Number getStartTimestamp() {
+        return startTimestamp;
+    }
+
+    public void setStartTimestamp(Number startTimestamp) {
+        this.startTimestamp = startTimestamp;
+    }
+
+    public Number getEndTimestamp() {
+        return endTimestamp;
+    }
+
+    public void setEndTimestamp(Number endTimestamp) {
+        this.endTimestamp = endTimestamp;
+    }
+
+    public Number getPage() {
+        return page;
+    }
+
+    public void setPage(Number page) {
+        this.page = page;
+    }
+
+    public Number getPagesize() {
+        return pagesize;
+    }
+
+    public void setPagesize(Number pagesize) {
+        this.pagesize = pagesize;
+    }
+
+    public String getOrder() {
+        return order;
+    }
+
+    public void setOrder(String order) {
+        this.order = order;
+    }
+}
