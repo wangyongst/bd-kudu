60d1483e4c894eced39f21325407a55c75813d41
wangyongst <wangyongst@gmail.com>
2018-08-09 14:20:19 +0800

    office

--- src/main/java/com/myweb/elasticsearch/service/OneService.java
+++ src/main/java/com/myweb/elasticsearch/service/OneService.java
@@ -1,8 +1,9 @@
 package com.myweb.elasticsearch.service;
 
-
+import com.anoyi.grpc.annotation.GrpcService;
 import com.myweb.vo.Parameter;
 
+@GrpcService(server = "OneGrpcService")
 public interface OneService {
 
     public Object queryDepthPriceRaw(Parameter parameter);
--- src/main/java/com/myweb/kafka/Listener.java
+++ src/main/java/com/myweb/kafka/Listener.java
@@ -23,9 +23,6 @@ public class Listener {
     public void depthPriceRaw(ConsumerRecord<?, ?> record) {
         ObjectMapper mapper = new ObjectMapper();
         try {
-            System.out.println("---------------------------------------------------");
-            System.out.println(record.value().toString());
-            System.out.println("---------------------------------------------------");
             DepthPriceRaw depthPriceRaw = mapper.readValue(record.value().toString(), DepthPriceRaw.class);
             depthPriceRawRepository.save(depthPriceRaw);
         } catch (IOException e) {
@@ -37,9 +34,6 @@ public class Listener {
     public void tradeHistoryRaw(ConsumerRecord<?, ?> record) {
         ObjectMapper mapper = new ObjectMapper();
         try {
-            System.out.println("---------------------------------------------------");
-            System.out.println(record.value().toString());
-            System.out.println("---------------------------------------------------");
             TradeHistoryRaw tradeHistoryRaw = mapper.readValue(record.value().toString(), TradeHistoryRaw.class);
             tradeHistoryRawRepository.save(tradeHistoryRaw);
         } catch (IOException e) {
