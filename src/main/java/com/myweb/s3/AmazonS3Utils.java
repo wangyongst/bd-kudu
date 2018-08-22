3e42d330d0aaa49049152cf8a7b8e7bf78e86eff
wangyongst <wangyongst@gmail.com>
2018-08-22 17:08:20 +0800

    office

--- src/main/java/com/myweb/s3/AmazonS3Utils.java
+++ src/main/java/com/myweb/s3/AmazonS3Utils.java
@@ -6,12 +6,13 @@ import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3Client;
 import com.amazonaws.services.s3.model.Bucket;
-import com.amazonaws.services.s3.model.CannedAccessControlList;
 import com.amazonaws.services.s3.model.PutObjectRequest;
+import com.amazonaws.services.s3.model.S3ObjectInputStream;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import java.io.File;
+import java.io.FileOutputStream;
 
 @Component
 public class AmazonS3Utils {
@@ -29,7 +30,23 @@ public class AmazonS3Utils {
         s3client = new AmazonS3Client(credentials);
     }
 
+    public void createFile(String bucketName, String fileName, File file) {
+        s3client.putObject(bucketName, fileName, file);
+    }
 
+    public File getFile(String bucketName, String fileName) throws Exception {
+        S3ObjectInputStream s3is = s3client.getObject(bucketName, fileName).getObjectContent();
+        File file = new File(fileName);
+        FileOutputStream fos = new FileOutputStream(file);
+        byte[] read_buf = new byte[1024];
+        int read_len = 0;
+        while ((read_len = s3is.read(read_buf)) > 0) {
+            fos.write(read_buf, 0, read_len);
+        }
+        s3is.close();
+        fos.close();
+        return file;
+    }
 
 
     public static void main(String[] args) {
@@ -41,7 +58,6 @@ public class AmazonS3Utils {
         }
         // create folder into bucket
         String folderName = "data-lake";
-
         // upload file to folder and set it to public
         String fileName = folderName + SUFFIX + "aaaa.avro";
         s3client2.putObject(new PutObjectRequest(bucketName, fileName, new File("C:\\Users\\Administrator\\Desktop\\aaaa.avro")));
