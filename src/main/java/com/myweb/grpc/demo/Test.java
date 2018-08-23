0a973f2405a144f3ec502de3361c8eb1f2f45117
wangyongst <wangyongst@gmail.com>
2018-08-23 17:53:35 +0800

    office

---
+++ src/main/java/com/myweb/grpc/demo/Test.java
@@ -0,0 +1,54 @@
+package com.myweb.grpc.demo;
+
+import com.amazonaws.services.s3.internal.ServiceUtils;
+import com.myweb.avro.AvroUtils;
+import com.myweb.domain.DepthPriceRaw;
+import com.myweb.domain.Price;
+import com.myweb.s3.S3Utils;
+import com.myweb.vo.Parameter;
+import org.apache.avro.file.DataFileReader;
+import org.apache.avro.file.DataFileWriter;
+
+import java.awt.print.Pageable;
+import java.io.File;
+import java.io.IOException;
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+public class Test {
+
+    public static void main(String[] args) {
+        DepthPriceRaw depthPriceRaw = new DepthPriceRaw();
+        depthPriceRaw.setSymbol("111");
+        depthPriceRaw.setCounterParty("1111");
+        depthPriceRaw.setTimestamp(new Date().getTime());
+        Price price = new Price();
+        price.setPrice(new BigDecimal(11111));
+        price.setQuantity(new BigDecimal(11111));
+        List<Price> priceList = new ArrayList<Price>();
+        priceList.add(price);
+        depthPriceRaw.setAsks(priceList);
+        depthPriceRaw.setBids(priceList);
+
+        Parameter parameter = new Parameter();
+        parameter.setEndTimestamp(depthPriceRaw.getTimestamp());
+        File file = S3Utils.makeTempFile(S3Utils.makePath(parameter),S3Utils.makeDepthPriceFileName(parameter,depthPriceRaw));
+        DataFileWriter<DepthPriceRaw> dataFileWriter = (DataFileWriter<DepthPriceRaw>) AvroUtils.getDataFileWriter(DepthPriceRaw.class, file);
+        AvroUtils.writeToAvro(dataFileWriter, depthPriceRaw, DepthPriceRaw.class);
+        AvroUtils.closeWriter(dataFileWriter);
+
+        DataFileReader<DepthPriceRaw> dataFileReader = (DataFileReader<DepthPriceRaw>) AvroUtils.getDataFileReader(file, DepthPriceRaw.class);
+        DepthPriceRaw depthPriceRaw2 = null;
+        while (dataFileReader != null && dataFileReader.hasNext()) {
+            try {
+                depthPriceRaw2 = dataFileReader.next(depthPriceRaw);
+                System.out.println(depthPriceRaw2.getTimestamp());
+            } catch (IOException e1) {
+                e1.printStackTrace();
+            }
+        }
+        AvroUtils.closeDataFileReader(dataFileReader);
+    }
+}
