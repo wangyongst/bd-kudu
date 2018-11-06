e2b6064c1726afed3298c03391912898f823bc2b
wangyongst <wangyongst@gmail.com>
2018-11-06 10:36:09 +0800

    office

--- src/main/java/com/boot/spring/boot/MainApplication.java
+++ src/main/java/com/boot/spring/boot/MainApplication.java
@@ -3,13 +3,11 @@ package com.boot.spring.boot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
 
 /**
  * Created by BHWL on 2017-08-09.
  */
 @SpringBootApplication(scanBasePackages = {"com.myweb"})
-@EnableElasticsearchRepositories(basePackages = {"com.myweb.elasticsearch.dao"})
 public class MainApplication {
     public static void main(String[] args) throws Exception {
         SpringApplication.run(MainApplication.class, args);
