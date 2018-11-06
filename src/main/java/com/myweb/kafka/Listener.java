4ce93fa346fa3ee31c0ea0ffa54e65f4aa254c0a
wangyongst <wangyongst@gmail.com>
2018-11-06 17:09:51 +0800

    office

--- src/main/java/com/myweb/kafka/Listener.java
+++ src/main/java/com/myweb/kafka/Listener.java
@@ -8,8 +8,15 @@ import org.springframework.kafka.annotation.KafkaListener;
 public class Listener {
 
     private static final Logger logger = LogManager.getLogger(Listener.class);
-    @KafkaListener(topics = {"test"})
-    public void test(ConsumerRecord<?, ?> record) {
+
+    @KafkaListener(topics = {"csv"})
+    public void csv(ConsumerRecord<?, ?> record) {
+        logger.info("kafka的key: " + record.key());
+        logger.info("kafka的value: " + record.value().toString());
+    }
+
+    @KafkaListener(topics = {"api"})
+    public void api(ConsumerRecord<?, ?> record) {
         logger.info("kafka的key: " + record.key());
         logger.info("kafka的value: " + record.value().toString());
     }
